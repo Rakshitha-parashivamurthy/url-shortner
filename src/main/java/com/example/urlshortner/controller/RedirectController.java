54622e09b10ea4dac3de1d6f502af5d611768e48
Rakshitha-parashivamurthy <rakshithap107@gmail.com>
2026-01-18 00:08:28 +0530

    Fix servlet package for Spring Boot 3 (jakarta)

--- src/main/java/com/example/urlshortner/controller/RedirectController.java
+++ src/main/java/com/example/urlshortner/controller/RedirectController.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import jakarta.servlet.http.HttpServletResponse;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
